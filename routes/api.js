f7900d7b64a6ec860904ee15499e4216c2f999e4
Aamir Ahmad <aamirahmad@outlook.com>
2016-11-20 17:06:00 -0500

    not my code

--- routes/api.js
+++ routes/api.js
@@ -7,59 +7,64 @@ var gcloud = require('gcloud');
 var path = require("path");
 var pricing = require('../pricing/pricing');
 var title_func = require('./title');
+var categorize_func = require('./categorize');
 
 var vision = gcloud.vision({
-  projectId: 'simplykijiji-150019',
-  keyFilename: path.resolve(__dirname, './keyfile.json')
+    projectId: 'simplykijiji-150019',
+    keyFilename: path.resolve(__dirname, './keyfile.json')
 });
 
 var filename = "";
 var UPLOAD_DIR = __dirname + "/../public/uploads/";
 
 var storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, UPLOAD_DIR)
-  },
-  filename: function (req, file, callback) {
-    filename = file.originalname;
-    callback(null, file.originalname);
-  }
+    destination: function(req, file, callback) {
+        callback(null, UPLOAD_DIR)
+    },
+    filename: function(req, file, callback) {
+        filename = file.originalname;
+        callback(null, file.originalname);
+    }
 });
 
 var upload = multer({
-  dest: UPLOAD_DIR,
-  limits: { fileSize: 3000000, files: 1 },
-  storage: storage
+    dest: UPLOAD_DIR,
+    limits: { fileSize: 3000000, files: 1 },
+    storage: storage
 });
 
-router.post('/image/upload', upload.any(), function (req, res, next) {
-  helper.getLocationFromEXIFData(filename, function (err, location) {
-    if (err) {
-      console.log(err);
-      location = null;
-    }
-    vision.detectLabels(UPLOAD_DIR + filename, function (err, labels, apiResponse) {
-      if (err) console.log(err);
-      var price_range = pricing.keysToPrices(labels);
-      title_func.titleAndDescriptionItem(labels, function (err, result) {
-        var response = {
-          title : result.title,
-          filepath: "/uploads/" + filename,
-          description: result.description,
-          catagories: labels,
-          price_range,
-          location,
+router.post('/image/upload', upload.any(), function(req, res, next) {
+    helper.getLocationFromEXIFData(filename, function(err, location) {
+        if (err) {
+            console.log(err);
+            location = null;
         }
-        console.log(response);
-        res.send(response);
-      });
+        vision.detectLabels(UPLOAD_DIR + filename, function(err, labels, apiResponse) {
+            if (err) console.log(err);
+            var price_range = pricing.keysToPrices(labels);
+            title_func.titleAndDescriptionItem(labels, function(err, result) {
+                if (err) console.log(err);
+                categorize_func.categorizeItem(labels, function(err, labelscat) {
+                    if (err) console.log(err);
+                    var response = {
+                        title: result.title,
+                        filepath: "/uploads/" + filename,
+                        description: result.description,
+                        catagories: labelscat,
+                        price_range,
+                        location,
+                    }
+                    console.log(response);
+                    res.send(response);
+                })
+            });
+        });
     });
-  });
 });
 
-router.get('/file', function (req, res, next) {
-  var r = pricing.keysToPrices(["toyota", "car"]);
-  res.send(r);
+router.get('/file', function(req, res, next) {
+    var r = pricing.keysToPrices(["toyota", "car"]);
+    res.send(r);
 });
 
 module.exports = router;
--- routes/categorize.js
+++ routes/categorize.js
@@ -1,39 +1,41 @@
 var Fuse = require('fuse.js');
 
-var kijijiCategories = ['animal, pet services', 
-									'birds for rehoming',
-									'cats, kittens for rehoming',
-									'dogs, puppies for rehoming',
-									'equestrian, livestock accessories',
-									'fish for rehoming',
-									'horses, ponies for rehoming',
-									'livestock',
-									'lost & found',
-									'accessories',
-									'reptiles & amphibians for rehoming',
-									'small animals for rehoming',
-									'other pets for rehoming',
-									'furniture',
-									'SUV',
-									'Trucks',
-									'Vans',
-									'Motorcycles',
-									'RV'];
+var kijijiCategories = ['animal, pet services',
+	'birds for rehoming',
+	'cats, kittens for rehoming',
+	'dogs, puppies for rehoming',
+	'equestrian, livestock accessories',
+	'fish for rehoming',
+	'horses, ponies for rehoming',
+	'livestock',
+	'lost & found',
+	'accessories',
+	'reptiles & amphibians for rehoming',
+	'small animals for rehoming',
+	'other pets for rehoming',
+	'furniture',
+	'SUV',
+	'Trucks',
+	'Vans',
+	'Motorcycles',
+	'RV'];
 
-function categorizeItem(labels, callback){
-		var searchCats = new Fuse(kijijiCategories);
-		var options = {
-			 shouldSort: true,
-			 threshold: 0.6,
-			 location: 0,
-			 distance: 100,
-			 maxPatternLength: 32,
-		};
-		for (var i = 0; i < labels.length; i++){
-			var category = searchCats.search(labels[i]);
-			for (var j = 0; j < category.length; j++)
-				console.log(kijijiCategories[category[j]]);
-		}
+module.exports.categorizeItem = function(labels, callback) {
+	var searchCats = new Fuse(kijijiCategories);
+	var options = {
+		shouldSort: true,
+		threshold: 0.6,
+		location: 0,
+		distance: 100,
+		maxPatternLength: 32,
+	};
+	var cat_result = new Array;
+	for (var i = 0; i < labels.length; i++) {
+		var category = searchCats.search(labels[i]);
+		
+		for (var j = 0; j < category.length; j++)
+			cat_result.push(kijijiCategories[category[j]]);
+	}
+	callback(null, cat_result);
+	
 }
-
-categorizeItem(['rehoming'], console.log);
