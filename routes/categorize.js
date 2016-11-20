64fbf91f470c6ac13c376f4b65b5ed09791c8be2
Yahya <yahya.ismail@hotmail.ca>
2016-11-20 16:01:16 -0500

    Adding title and description capabilities as well as NLP

--- routes/categorize.js
+++ routes/categorize.js
@@ -1,33 +1,39 @@
 var Fuse = require('fuse.js');
 
-var kijijiCategories = ["animal, pet services", 
-									"birds for rehoming",
-									"cats, kittens for rehoming",
-									"dogs, puppies for rehoming",
-									"equestrian, livestock accessories",
-									"fish for rehoming",
-									"horses, ponies for rehoming",
-									"livestock",
-									"lost & found",
-									"accessories",
-									"reptiles & amphibians for rehoming",
-									"small animals for rehoming",
-									"other pets for rehoming",
-									"furniture",
-									"SUV",
-									"Trucks",
-									"Vans",
-									"Motorcycles",
-									"RV"];
+var kijijiCategories = ['animal, pet services', 
+									'birds for rehoming',
+									'cats, kittens for rehoming',
+									'dogs, puppies for rehoming',
+									'equestrian, livestock accessories',
+									'fish for rehoming',
+									'horses, ponies for rehoming',
+									'livestock',
+									'lost & found',
+									'accessories',
+									'reptiles & amphibians for rehoming',
+									'small animals for rehoming',
+									'other pets for rehoming',
+									'furniture',
+									'SUV',
+									'Trucks',
+									'Vans',
+									'Motorcycles',
+									'RV'];
 
 function categorizeItem(labels, callback){
 		var searchCats = new Fuse(kijijiCategories);
-		
+		var options = {
+			 shouldSort: true,
+			 threshold: 0.6,
+			 location: 0,
+			 distance: 100,
+			 maxPatternLength: 32,
+		};
 		for (var i = 0; i < labels.length; i++){
-			var category = kijijiCategories[searchCats.search(labels[i])];
-			if (category != undefined)
-				console.log(category);
+			var category = searchCats.search(labels[i]);
+			for (var j = 0; j < category.length; j++)
+				console.log(kijijiCategories[category[j]]);
 		}
 }
 
-categorizeItem(["vertebrate"], console.log);
+categorizeItem(['rehoming'], console.log);
